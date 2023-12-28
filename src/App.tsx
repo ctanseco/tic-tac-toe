d612f3635a3419d4377261031eaf4c848e87ffe6
casimero tanseco <ctanseco@gmail.com>
2023-12-27 19:19:16 -0800

    removed redundant props

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,10 @@ interface SquareProps {
   onSquareClick: () => void
 }
 
-function Square (props: SquareProps): React.ReactElement {
+function Square ({ value, onSquareClick }: SquareProps): React.ReactElement {
   return (
-    <button className="square" onClick={props.onSquareClick}>
-      {props.value}
+    <button className="square" onClick={onSquareClick}>
+      {value}
     </button>
   )
 }
@@ -19,45 +19,45 @@ interface BoardProps {
   onPlay: (squares: string[]) => void
 }
 
-function Board (props: BoardProps): React.ReactElement {
+function Board ({ xIsNext, squares, onPlay }: BoardProps): React.ReactElement {
   function handleClick (i: number): void {
-    if (calculateWinner(props.squares) !== '' || props.squares[i] !== null) {
+    if (calculateWinner(squares) !== '' || squares[i] !== null) {
       return
     }
-    const nextSquares: string[] = props.squares.slice()
-    if (props.xIsNext) {
+    const nextSquares: string[] = squares.slice()
+    if (xIsNext) {
       nextSquares[i] = 'X'
     } else {
       nextSquares[i] = '0'
     }
-    props.onPlay(nextSquares)
+    onPlay(nextSquares)
   }
 
-  const winner = calculateWinner(props.squares)
+  const winner = calculateWinner(squares)
   let status: string
   if (winner !== null) {
     status = 'Winner: ' + winner
   } else {
-    status = 'Next Player: ' + (props.xIsNext ? 'X' : 'O')
+    status = 'Next Player: ' + (xIsNext ? 'X' : 'O')
   }
 
   return (
     <>
       <div className="status">{status}</div>
       <div className="board-row">
-        <Square value={props.squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={props.squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={props.squares[2]} onSquareClick={() => handleClick(2)} />
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square value={props.squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={props.squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={props.squares[5]} onSquareClick={() => handleClick(5)} />
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square value={props.squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={props.squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={props.squares[8]} onSquareClick={() => handleClick(8)} />
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   )
